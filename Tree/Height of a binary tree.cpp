ef6659366230ec185177bf016fb0bfdf1c2c8936
Gursimran Singh <gursimransingh154@gmail.com>
2021-03-29 21:17:13 +0530

    Update Height of a binary tree.cpp

--- Tree/Height of a binary tree.cpp
+++ Tree/Height of a binary tree.cpp
@@ -38,21 +38,21 @@ class Node {
     Node* right;
 };
 */
-    int height(Node* root) {
-        if(root == NULL){
-            return -1;
+int height(Node* root) {
+    if(root == NULL){
+        return -1;
+    }
+    else{
+        int ldepth = height(root -> left);
+        int rdepth = height(root -> right);
+        if(ldepth > rdepth){
+            return (ldepth + 1);
         }
         else{
-            int ldepth = height(root -> left);
-            int rdepth = height(root -> right);
-            if(ldepth > rdepth){
-                return (ldepth + 1);
-            }
-            else{
-                return (rdepth + 1);
-            }
+            return (rdepth + 1);
         }
     }
+}
 
 }; //End of Solution
 
