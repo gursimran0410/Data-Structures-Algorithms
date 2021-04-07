b7a818a8583db6ef6b5e7649af493c02b67a9e11
Gursimran Singh <gursimransingh154@gmail.com>
2021-04-07 23:05:21 +0530

    Create Staircase.cpp

---
+++ Algorithms/Staircase.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the staircase function below.
+void staircase(int n) {
+    for(int i=n;i>0;i--){
+        for(int j=0;j<i-1;j++){
+            cout << " ";
+        }
+        for(int k=0;k<n-i+1;k++){
+            cout << "#";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
+}
