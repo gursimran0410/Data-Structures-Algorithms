42c47b768a49b48384885fb25323635d21b67799
Gursimran Singh <gursimransingh154@gmail.com>
2021-06-02 22:50:04 +0530

    Update Array: Left Rotation.cpp

--- Algorithms/Array: Left Rotation.cpp
+++ Algorithms/Array: Left Rotation.cpp
@@ -33,7 +33,6 @@ int main()
     getline(cin, first_multiple_input_temp);
     vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
     int n = stoi(first_multiple_input[0]);
-
     int d = stoi(first_multiple_input[1]);
 
     string a_temp_temp;
