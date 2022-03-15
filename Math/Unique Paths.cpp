7190ffc354a82814983d75af5057b42b001b7093
Gursimran Singh <gursimransingh154@gmail.com>
2022-03-15 15:54:51 +0530

    Unique Paths
    Input: m = 3, n = 2
    Output: 3
    Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
    1. Right -> Down -> Down
    2. Down -> Down -> Right
    3. Down -> Right -> Down

---
+++ Math/Unique Paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int NoOfPaths = n+m-2;
+        int r = m-1;
+        double res = 1;
+        for(int i=1; i<=r; i++){
+            res = res * (NoOfPaths - r + i)/i;
+        }
+        return (int)res;
+    }
+};
