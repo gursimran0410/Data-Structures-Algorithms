09d5431c5089984bcb0890d67ef44efa3644432e
Gursimran Singh <gursimransingh154@gmail.com>
2022-03-17 11:52:17 +0530

    Update Longest Consecutive Sequence.cpp

--- Arrays/Longest Consecutive Sequence.cpp
+++ Arrays/Longest Consecutive Sequence.cpp
@@ -6,9 +6,11 @@ public:
             hashSet.insert(num);
         }
         int longestStreak = 0;
-        for(int num:nums){
-            if(!hashSet.count(num-1)){
-                int currentNum = num;
+        
+        unordered_set<int> :: iterator num;
+        for(num = hashSet.begin(); num != hashSet.end(); num++){
+            if(!hashSet.count(*num-1)){
+                int currentNum = *num;
                 int currentStreak = 1;
                 while(hashSet.count(currentNum+1)){
                     currentNum++;
