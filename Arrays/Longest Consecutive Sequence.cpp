f2a147de04bcbbdc1386bb322090060949322134
Gursimran Singh <gursimransingh154@gmail.com>
2022-03-17 11:38:06 +0530

    Longest Consecutive Sequence
    Example 1:
    
    Input: nums = [100,4,200,1,3,2]
    Output: 4
    Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
    Example 2:
    
    Input: nums = [0,3,7,2,5,8,4,6,0,1]
    Output: 9

---
+++ Arrays/Longest Consecutive Sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> hashSet;
+        for(int num:nums){
+            hashSet.insert(num);
+        }
+        int longestStreak = 0;
+        for(int num:nums){
+            if(!hashSet.count(num-1)){
+                int currentNum = num;
+                int currentStreak = 1;
+                while(hashSet.count(currentNum+1)){
+                    currentNum++;
+                    currentStreak++;
+                }
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+};
