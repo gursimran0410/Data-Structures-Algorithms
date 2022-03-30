de9d799818865ae6576b70f0b1a7e1c7ab3cd1e0
Gursimran Singh <gursimransingh154@gmail.com>
2022-03-30 12:01:21 +0530

    Minimum Remove to Make Valid Parentheses
    Example 1:
    
    Input: s = "lee(t(c)o)de)"
    Output: "lee(t(c)o)de"
    Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
    Example 2:
    
    Input: s = "a)b(c)d"
    Output: "ab(c)d"
    Example 3:
    
    Input: s = "))(("
    Output: ""
    Explanation: An empty string is also valid.

---
+++ Strings/Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stk;
+        
+        for(auto i=0; i<s.size(); ++i){
+            if(s[i] == '(') stk.push(i);
+            else if(s[i] == ')'){
+                if(!stk.empty()) stk.pop();
+                else s[i] = '*';
+            }
+        }
+        
+        while(!stk.empty()){
+            s[stk.top()] = '*';
+            stk.pop();
+        }
+        
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};
