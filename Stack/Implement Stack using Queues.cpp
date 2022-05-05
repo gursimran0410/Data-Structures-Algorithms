fed8b8ed7b4a29317690662098f2e70513b33417
Gursimran Singh <gursimransingh154@gmail.com>
2022-05-05 13:33:52 +0530

    Implement Stack using Queues
    Input
    ["MyStack", "push", "push", "top", "pop", "empty"]
    [[], [1], [2], [], [], []]
    Output
    [null, null, null, 2, 2, false]
    
    Explanation
    MyStack myStack = new MyStack();
    myStack.push(1);
    myStack.push(2);
    myStack.top(); // return 2
    myStack.pop(); // return 2
    myStack.empty(); // return False

---
+++ Stack/Implement Stack using Queues.cpp
@@ -0,0 +1,38 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+    }
+    
+    int pop() {
+        int size = q.size()-1;
+        while(size--){
+            int el1 = q.front();
+            q.pop();
+            q.push(el1);   
+        }
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
+    int top() {
+        return q.back();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
